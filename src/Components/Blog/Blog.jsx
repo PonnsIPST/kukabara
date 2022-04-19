8ca96e24363a035b38e9beb71bf16387aeff00a8
PNS <ensperon@gmail.com>
2022-04-20 01:37:26 +0700

    added search

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -3,31 +3,53 @@ import Btn from "../../UI/Btn/Btn";
 import Input from "../../UI/Input/Input";
 import Item from "../Item/Item";
 import Select from "../../UI/Select/Select";
+import { useMemo } from "react";
 
 
 const Blog = function () {
-    let now = new Date();
+
+
     const [items, setItems] = useState([
         { id: 1, title: 'b', body: 'Текст айтема', addInfo: 'Дополнительное поле', buttonText: '1', date: new Date('December 17, 1995 03:24:00') },
         { id: 2, title: 'a', body: 'Текст айтема', addInfo: 'Дополнительное поле', buttonText: '2', date: new Date('December 17, 1996 03:24:00') },
         { id: 3, title: 'c', body: 'Текст айтема', addInfo: 'Дополнительное поле', buttonText: '12 000 p.', date: new Date('December 17, 2000 03:24:00') }
     ])
+
+
     const removeItem = (item) => {
         setItems(items.filter(p => p.id !== item.id))
     }
+
+
     const sortItems = (sort) => {
         setSelectedSort(sort);
-        console.log(sort);
-        if (sort === 'date') {
-            setItems([...items].sort((a, b) => a.date.valueOf() - b.date.valueOf()));
-        }
-        else setItems([...items].sort((a, b) => a[sort].localeCompare(b[sort])))
     }
-    const [selectedSort, setSelectedSort] = useState('')
+
+
+    const [selectedSort, setSelectedSort] = useState('date')
     const [title, setTitle] = useState('')
     const [buttonText, setButtonText] = useState('')
     const [body, setBody] = useState('')
     const [addInfo, setAddInfo] = useState('')
+
+
+    const sortedArray = useMemo(() => {
+        console.log('работаем');
+        if (selectedSort) {
+            if (selectedSort === 'date') {
+                return [...items].sort((a, b) => a.date.valueOf() - b.date.valueOf());
+            }
+            return [...items].sort((a, b) => a[selectedSort].localeCompare(b[selectedSort]));
+        }
+        return items;
+    }, [selectedSort, items]);
+
+    const [searchQuery, setSearchQuery] = useState('');
+
+    const sortAndSearch = useMemo(() => {
+        return sortedArray.filter(item => item.title.toLowerCase().includes(searchQuery.toLowerCase()))
+    }, [searchQuery, sortedArray])
+
     const addNewItem = (e) => {
         e.preventDefault()
         const nowaday = new Date()
@@ -45,17 +67,25 @@ const Blog = function () {
         setAddInfo('')
         setButtonText('')
     }
+
+    function getSortedItems() {
+        
+    }
+
     if(items.length !== 0)
     return (
         <section>
             <h2>Свежее из нашего блога</h2>
             <div className="container">
-                <div className="row"><Select value={selectedSort} onChange={sortItems} defValue={'Сортировать'} options={[
+                <div className="row">
+                   <Select value={selectedSort} onChange={sortItems} defValue={'Сортировать'} options={[
                     { value: 'title', name: 'По названию' },
                     { value: 'date', name: 'По дате' }
-                ]} /></div>
+                    ]} />
+                    <Input value={searchQuery} onChange={e => setSearchQuery(e.target.value)} placeholder="Поиск" />
+                </div>
                 <div className="row">
-                    {items.map((item) =>
+                    {sortAndSearch.map((item) =>
                         <Item remove={removeItem} item={item} key={item.id}/>
                     )}
                 </div>
