0eb7a71824a60294c23541fffee965f4b0926c3d
PNS <ensperon@gmail.com>
2022-04-29 07:46:08 +0700

    fixed some lil bugs

--- src/Pages/Users.jsx
+++ src/Pages/Users.jsx
@@ -1,25 +1,28 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
 import { fetchUsers } from "../Async/users";
 import "../style/style.css";
+import Btn from "../UI/Btn/Btn";
 
 function Users(props) {
     var dispatch = useDispatch()
-    useEffect(() => {
-        dispatch(fetchUsers())
-    }, []);
     var allUsers = useSelector(state => state.userServer.users);
     console.log(allUsers)
     return (
-        <div style={{ display: `flex`, flexDirection: `row`, flexWrap: `wrap`, width: `80%`, marginLeft: `10%`, height: `65vh`, alignContent: `center` }}>
+        <div>
+                        <Btn onClick={() => dispatch(fetchUsers())}>Download users</Btn>
+        <div style={{ display: `flex`, flexDirection: `row`, flexWrap: `wrap`, width: `80%`, marginLeft: `10%`, minHeight: `65vh`, alignContent: `center` }}>
             {allUsers.map((user) => {
-            return(
-                <div style={{display: `flex`, width: `50%`, border: `1px solid #141414`, padding: `15px 20px 10px 15px`}}>
-                    <h3>{user.id}.{user.name}</h3>
-                </div>
-            )
+                
+                    return(
+                        <div style={{display: `flex`, width: `50%`, border: `1px solid #141414`, padding: `15px 20px 10px 15px`}}>
+                            <h3>{user.id}.{user.name}</h3>
+                        </div>
+                    )
+            
             })}
+            </div>
         </div>
     );
 }
